457f231c797cc533a1160b96aa2d12411e9eb1cc
Glow <garrettlindow42@gmail.com>
2022-10-07 13:00:45 -0400

    Full profiles styled

--- src/routes/Who-We-Are.svelte
+++ src/routes/Who-We-Are.svelte
@@ -16,41 +16,43 @@
 	work to make it happen so far. As we continue to grow, more community members are becoming
 	involved in supporting this work!
 </p>
-<div>
-	<h1>Payge Lindow</h1>
-	<Divide />
-	<p>
-		Payge has a B.S. in Biology from the University of Michigan, where she also studied sustainable
-		food systems, gender, and public health. She is a first generation, young, and queer farmer and
-		faciliator from southeast Michigan. She created an urban farm last year for a nonprofit and led
-		education programs for K-10 students, which fueled her passion for liberating our food. Payge is
-		the Michigan Organizer for the National Young Farmers Coalition, where she uplifts farmers’ work
-		and voices into policy conversations, while facilitating community building between farmers
-		across the Midwest.
-	</p>
-</div>
+<div class="border-4 border-white rounded-lg bg-brown/20 m-4 p-4">
+	<div class="flex w-full items-center align-middle pb-2 ">
+		<img
+			class="mr-8 h-24 w-24 md:h-32 md:w-32 border-white border-4 rounded-full"
+			src="/Visser.png"
+			alt="Elizabeth Visser"
+		/>
+		<div class="w-full">
+			<h1 class="max-w-full">Elizabeth Visser</h1>
+			<Divide />
+		</div>
+	</div>
 
-<div>
-	<h1>Mikayla Rowden</h1>
-	<Divide />
 	<p>
-		My name is Mik (she/they). I am a first generation, queer, young farmer in the West Michigan
-		area. Two seasons ago I started a small farm called <a href="https://www.stillwindfarmllc.com/"
-			>Stillwind</a
-		>, which is now a 1/2 acre, low till, Certified Naturally Grown operation. I also work full time
-		at Full Hollow Farm & volunteer on both the West MI Young Farmers Chapter &
-		<a href="https://www.wmgrowersgroup.org/">West MI Growers Group</a> boards. I find joy & rest while
-		enjoying meals with others, reading, & spending time outside!
+		My name is Elizabeth, though I go by Liz. I am not the first person in my family to farm, though
+		I am the only person in my family who is farming as a profession currently. I manage the
+		Blandford Nature Center Farm in Grand Rapids, and have been working there since 2016.
 	</p>
 	<p>
-		With plentiful barriers to entry & systematic issues, I find it is inspiring to work alongside
-		local farmers, community members, & leaders to hopefully cultivate a more equitable food system
-		& industry for young, BIPOC, queer, aspiring growers.
+		I received a B.A. from Grand Valley State University with an emphasis in Illustration. The more
+		I learn, whether it’s about soil management, a new knitting technique, or making connections in
+		the community, the more I see how everything is interconnected. I believe in reciprocity, and
+		that the creation of an equitable food system benefits everyone.
 	</p>
 </div>
-<div>
-	<h1>Alita Kelly</h1>
-	<Divide />
+<div class="border-4 border-white rounded-lg bg-brown/20 m-4 p-4">
+	<div class="flex w-full items-center align-middle pb-2 ">
+		<img
+			class="mr-8 h-24 w-24 md:h-32 md:w-32 border-white border-4 rounded-full"
+			src="/Kelly.png"
+			alt="Alita Kelly"
+		/>
+		<div class="w-full">
+			<h1 class="max-w-full">Alita Kelly</h1>
+			<Divide />
+		</div>
+	</div>
 	<p>
 		Alita received a B.S. from University of Michigan in Environmental Science and Sustainable
 		Business. She is the founder of South East Market, a sustainable, equitable, people-powered
@@ -67,5 +69,54 @@
 		In her free time you can find Alita with her partner, Sergio, and their two daughters, Willow
 		and Alondra, traveling, observing insects, playing soccer and enjoying the great outdoors and
 		good company.
+	</p>
+</div>
+<div class="border-4 border-white rounded-lg bg-brown/20 m-4 p-4">
+	<div class="flex w-full items-center align-middle pb-2 ">
+		<img
+			class="mr-8 h-24 w-24 md:h-32 md:w-32 p border-white border-4 rounded-full"
+			src="/Lindow.png"
+			alt="Payge Lindow"
+		/>
+		<div class="w-full">
+			<h1 class="max-w-full">Payge Lindow</h1>
+			<Divide />
+		</div>
+	</div>
+	<p>
+		Payge has a B.S. in Biology from the University of Michigan, where she also studied sustainable
+		food systems, gender, and public health. She is a first generation, young, and queer farmer and
+		faciliator from southeast Michigan. She created an urban farm last year for a nonprofit and led
+		education programs for K-10 students, which fueled her passion for liberating our food. Payge is
+		the Michigan Organizer for the National Young Farmers Coalition, where she uplifts farmers’ work
+		and voices into policy conversations, while facilitating community building between farmers
+		across the Midwest.
+	</p>
+</div>
+<div class="border-4 border-white rounded-lg bg-brown/20 m-4 p-4">
+	<div class="flex w-full items-center align-middle pb-2 ">
+		<img
+			class="mr-8 h-24 w-24 md:h-32 md:w-32 border-white border-4 rounded-full"
+			src="/Rowden.png"
+			alt="Mikayla Rowden"
+		/>
+		<div class="w-full">
+			<h1 class="max-w-full">Mikayla Rowden</h1>
+			<Divide />
+		</div>
+	</div>
+	<p>
+		My name is Mik (she/they). I am a first generation, queer, young farmer in the West Michigan
+		area. Two seasons ago I started a small farm called <a href="https://www.stillwindfarmllc.com/"
+			>Stillwind</a
+		>, which is now a 1/2 acre, low till, Certified Naturally Grown operation. I also work full time
+		at Full Hollow Farm & volunteer on both the West MI Young Farmers Chapter &
+		<a href="https://www.wmgrowersgroup.org/">West MI Growers Group</a> boards. I find joy & rest while
+		enjoying meals with others, reading, & spending time outside!
+	</p>
+	<p>
+		With plentiful barriers to entry & systematic issues, I find it is inspiring to work alongside
+		local farmers, community members, & leaders to hopefully cultivate a more equitable food system
+		& industry for young, BIPOC, queer, aspiring growers.
 	</p>
 </div>
